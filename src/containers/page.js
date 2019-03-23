c54c12898fa650d06dd883a4f412b34e499b98ee
Timothy Keaveny <tim@reflexions.co>
2019-03-23 14:34:35 -0400

    Add "page.js" which will act as a index for all container components

---
+++ src/containers/page.js
@@ -0,0 +1,10 @@
+import withSidebarLayout from './withSidebarLayout';
+import withRouter from './withRouter';
+
+export const pageWithoutLayout = Wrapped => {
+    return withRouter(Wrapped);
+}
+
+export const pageWithLayout = Wrapped => withSidebarLayout(Wrapped)
+
+export default pageWithLayout;
