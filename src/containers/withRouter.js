8b942d410a07dd5ebd416f7e04f4eef102b2fc54
Timothy Keaveny <tim@reflexions.co>
2019-03-23 14:45:19 -0400

    Add the "withRouter.js" container component that integrates the "Routes" component into a wrapper

---
+++ src/containers/withRouter.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export const withRouter = Wrapped => props => (
+    <Router>
+        <Wrapped {...props} />
+    </Router>
+)
+
+export default withRouter;
